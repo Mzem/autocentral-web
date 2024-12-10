922fbf1e67c1e87a4655633ec9a620ed827117f4
Malek Zemni <malek.zemni@octo.com>
2024-12-10 16:59:38 +0100

    add infolinks

--- app/layout.tsx
+++ app/layout.tsx
@@ -196,6 +196,14 @@ export default function RootLayout({
         <Footer />
         <Analytics />
         <SpeedInsights />
+        <script type='text/javascript'>
+          {' '}
+          var infolinks_pid = 3429875; var infolinks_wsid = 0;{' '}
+        </script>{' '}
+        <script
+          type='text/javascript'
+          src='//resources.infolinks.com/js/infolinks_main.js'
+        ></script>
       </body>
     </html>
   )
